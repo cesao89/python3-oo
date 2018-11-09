499e8f37d6fb6dce82be5b68e2f11cb1fad28fee
Cesar Domingos <cesar.domingos@fs.com.br>
2018-11-09 13:25:55 -0200

    Aula 5

--- conta.py
+++ conta.py
@@ -1,7 +1,5 @@
 
-
 class Conta:
-
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
         self.__numero = numero
@@ -9,18 +7,34 @@ class Conta:
         self.__saldo = saldo
         self.__limite = limite
 
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
 
     def extrato(self):
         print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
-
     def deposita(self, valor):
         self.__saldo += valor
 
-
     def saca(self, valor):
         self.__saldo -= valor
-
 
     def transfere(self, valor, destino):
         self.saca(valor)
