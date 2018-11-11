5b8ef5b85672eb4d9c4c4ec3884a26b749a318e6
Cesar Domingos <cesar_web@live.com>
2018-11-10 23:14:24 -0200

    Aula 6

--- conta.py
+++ conta.py
@@ -1,5 +1,5 @@
-
 class Conta:
+
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
         self.__numero = numero
@@ -27,6 +27,14 @@ class Conta:
     def limite(self, limite):
         self.__limite = limite
 
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {"BB": "001", "Caixa": "104", "Bradesco": "237"}
+
     def extrato(self):
         print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
@@ -34,8 +42,15 @@ class Conta:
         self.__saldo += valor
 
     def saca(self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite para saque".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
